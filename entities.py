df76a708f3ba03eaad25c19d66a0c6686e49f5f9
Daniel <daniel@gmail.com>
2015-04-22 19:03:20 -0700

    all miner working 4 entity

--- entities.py
+++ entities.py
@@ -1,4 +1,8 @@
-
+#I honestly just couldn't figure out
+#the syntax in time. I spent so much
+#time working on this but I couldn't
+#figure it out by myself without
+#spending a ton of time
 
 import point
 
@@ -11,18 +15,13 @@ class Background:
       return self.imgs
 
 class Entity(object):
-   def __init__(self, name, resource_limit, position, rate, imgs,
-      animation_rate):
+   def __init__(self,name,position,rate,imgs):
       self.name = name
       self.position = position
       self.rate = rate
       self.imgs = imgs
-      self.current_img = 0    
-      self.resource_limit = resource_limit
-      self.resource_count = 0
-      self.animation_rate = animation_rate
-      self.pending_actions = []
-   def set_position(self, point):
+      self.current_img = 0
+   def set_position(self,point):
       self.position = point
    def get_position(self):
       return self.position
@@ -30,16 +29,58 @@ class Entity(object):
       return self.imgs
    def get_rate(self):
       return self.rate
+   def get_name(self):
+      return self.name
+class MinerNotFull(Entity):
+   def __init__(self, name, resource_limit, position, rate, imgs,
+      animation_rate):
+      self.current_img = 0
+      self.resource_limit = resource_limit
+      self.resource_count = 0
+      self.animation_rate = animation_rate
+      self.pending_actions = []
+      super(MinerNotFull,self).__init__(name,position,rate,imgs)
    def set_resource_count(self, n):
       self.resource_count = n
    def get_resource_count(self):
       return self.resource_count
    def get_resource_limit(self):
       return self.resource_limit
-   def get_resource_distance(self):
-      return self.resource_distance
-   def get_name(self):
-      return self.name
+   def get_animation_rate(self):
+      return self.animation_rate
+   def remove_pending_action(self, action):
+      if hasattr(self, "pending_actions"):
+         self.pending_actions.remove(action)
+   def add_pending_action(self, action):
+      if hasattr(self, "pending_actions"):
+         self.pending_actions.append(action) 
+   def get_pending_actions(self):
+      if hasattr(self, "pending_actions"):
+          return self.pending_actions
+      else:
+          return [] 
+   def clear_pending_actions(self):
+      if hasattr(self, "pending_actions"):
+          self.pending_actions = []
+   def entity_string(self):                                                       
+      return ' '.join(['miner', self.name, str(self.position.x),
+          str(self.position.y), str(self.resource_limit),
+          str(self.rate), str(self.animation_rate)])
+class MinerFull(Entity):
+   def __init__(self, name, resource_limit, position, rate, imgs,
+      animation_rate):
+      self.current_img = 0
+      self.resource_limit = resource_limit
+      self.resource_count = resource_limit
+      self.animation_rate = animation_rate
+      self.pending_actions = []
+      super(MinerFull,self).__init__(name,position,rate,imgs)
+   def set_resource_count(self, n):
+      self.resource_count = n
+   def get_resource_count(self):
+      return self.resource_count
+   def get_resource_limit(self):
+      return self.resource_limit
    def get_animation_rate(self):
       return self.animation_rate
    def remove_pending_action(self, action):
@@ -56,25 +97,6 @@ class Entity(object):
    def clear_pending_actions(self):
       if hasattr(self, "pending_actions"):
           self.pending_actions = []
-
-class MinerNotFull(Entity):
-   def __init__(self, name, resource_limit, position, rate, imgs,
-      animation_rate):
-      self.current_img = 0
-      self.resource_count = 0
-      self.pending_actions = []   
-      super(MinerNotFull,self).__init__(name,resource_limit,position,rate,imgs,animation_rate)
-   def entity_string(self):                                                         
-      return ' '.join(['miner', self.name, str(self.position.x),
-          str(self.position.y), str(self.resource_limit),
-          str(self.rate), str(self.animation_rate)])
-
-class MinerFull(Entity):
-   def __init__(self, name, resource_limit, position, rate, imgs,
-      animation_rate):
-      self.current_img = 0
-      self.pending_actions = []
-      super(MinerFull,self).__init__(name,resource_limit,position,rate,imgs,animation_rate)
 class Vein:
    def __init__(self, name, rate, position, imgs, resource_distance=1):
       self.name = name
@@ -140,6 +162,8 @@ class Ore:
       return self.rate
    def set_resource_count(self, n):
       self.resource_count = n
+   def get_resource_count(self):
+      return self.resource_count
    def get_resource_limit(self):
       return self.resource_limit
    def get_resource_distance(self):
